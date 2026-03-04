a74e1ff87a728dd8e1715bafa4697c2644e74710
Benjamin00 <benjaminhill00@gmail.com>
2026-03-03 23:17:32 -0600

    Removing GH-pages, switching to Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/Randort" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/Randort/" : "",
   images: {
     remotePatterns: [
       {
